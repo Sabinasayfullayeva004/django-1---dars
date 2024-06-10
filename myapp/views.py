c28a277dd8aff161ec5ce3a1c5c5be30fe531447
Sabinasayfullayeva004 <sabinasayfullayeva906@gmail.com>
2024-06-10 10:59:42 +0500

    django 1 - dars

--- myapp/views.py
+++ myapp/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+
+
+def hello_world(requist):
+    return HttpResponse("<h1>Assalomu alaykum Sifat</h1>")
